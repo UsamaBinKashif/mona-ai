e514911077f2aac57c2fd019930a050fb6f92443
Usama Ahmed <usamabinkashif.98@gmail.com>
2024-09-05 20:16:19 +0500

    mona-ai

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,29 @@
+import MeetingTypeList from '@/components/MeetingTypeList';
+
+const Home = () => {
+  const now = new Date();
+
+  const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
+  const date = (new Intl.DateTimeFormat('en-US', { dateStyle: 'full' })).format(now);
+
+  return (
+    <section className="flex size-full flex-col gap-5 text-white ">
+      <div className=" w-full rounded-[20px] ">
+        <div className="flex h-full flex-col justify-between  gap-y-5 ">
+          <div className="flex flex-col gap-1">
+            <h1 className="text-xl font-bold ">{time}</h1>
+            <p className="text-base font-medium text-sky-1">{date}</p>
+          </div>
+          <div className="flex flex-col gap-2">
+            <h1 className="text-4xl font-extrabold ">Video calls and meetings for everyone.</h1>
+            <p className="text-lg font-medium text-sky-1 ">Connect, collaborate, and celebrate from anywhere.</p>
+          </div>
+        </div>
+      </div>
+
+      <MeetingTypeList />
+    </section>
+  );
+};
+
+export default Home;
---
+++ app/layout.tsx
@@ -0,0 +1,48 @@
+/* eslint-disable camelcase */
+import { ReactNode } from "react";
+import type { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
+
+import "@stream-io/video-react-sdk/dist/css/styles.css";
+import "react-datepicker/dist/react-datepicker.css";
+import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
+
+
+
+export const metadata: Metadata = {
+  title: "MONA AI",
+  description: "Video calling App",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: ReactNode }>) {
+  return (
+    <html lang="en">
+      <ClerkProvider
+        appearance={{
+          layout: {
+            socialButtonsVariant: "iconButton",
+            logoImageUrl: "/icons/yoom-logo.svg",
+          },
+          variables: {
+            colorText: "#fff",
+            colorPrimary: "#0E78F9",
+            colorBackground: "#1C1F2E",
+            colorInputBackground: "#252A41",
+            colorInputText: "#fff",
+          },
+        }}
+      >
+        <body className={` bg-dark-2`}>
+          <Toaster />
+          {children}
+        </body>
+      </ClerkProvider>
+    </html>
+  );
+}
