e1a62771bfacd4c65a6c79bcaa03ac2b5307c1c6
Usama Ahmed <usamabinkashif.98@gmail.com>
2024-09-05 20:27:31 +0500

    mona-ai

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,17 +1,19 @@
 'use client';
-import { useState } from 'react';
 import {
   CallControls,
   CallParticipantsList,
-  CallStatsButton,
   CallingState,
   PaginatedGridLayout,
   SpeakerLayout,
-  useCallStateHooks,
+  useCallStateHooks
 } from '@stream-io/video-react-sdk';
+import { LayoutList, Users } from 'lucide-react';
 import { useRouter, useSearchParams } from 'next/navigation';
-import { Users, LayoutList } from 'lucide-react';
+import { useState } from 'react';
 
+import { cn } from '@/lib/utils';
+import EndCallButton from './EndCallButton';
+import Loader from './Loader';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -19,9 +21,6 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from './ui/dropdown-menu';
-import Loader from './Loader';
-import EndCallButton from './EndCallButton';
-import { cn } from '@/lib/utils';
 
 type CallLayoutType = 'grid' | 'speaker-left' | 'speaker-right';
 
@@ -88,7 +87,6 @@ const MeetingRoom = () => {
             ))}
           </DropdownMenuContent>
         </DropdownMenu>
-        {/* <CallStatsButton /> */}
         <button onClick={() => setShowParticipants((prev) => !prev)}>
           <div className=" cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]  ">
             <Users size={20} className="text-white" />
