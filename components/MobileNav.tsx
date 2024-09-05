17a546f0fc4d097ca612e20150acf904edd5c21e
Usama Ahmed <usamabinkashif.98@gmail.com>
2024-09-05 20:34:32 +0500

    mona-ai

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -26,12 +26,11 @@ const MobileNav = () => {
         <SheetContent side="left" className="border-none bg-dark-1">
           <Link href="/" className="flex items-center gap-1">
             <Image
-              src="/icons/logo.svg"
+              src="/images/logo.png"
               width={32}
               height={32}
-              alt="yoom logo"
+              alt="logo"
             />
-            <p className="text-[26px] font-extrabold text-white">YOOM</p>
           </Link>
           <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
             <SheetClose asChild>
